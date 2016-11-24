9726d2c06974c47142211e7ce17f27555aed103d
Daniel Chen <team6612@gmail.com>
2016-11-25 01:09:36 +0800

    add comments

--- femsolver/femsolver.go
+++ femsolver/femsolver.go
@@ -14,12 +14,16 @@ type FEMsolver interface {
   Solve()
 }
 
+// Build a vector from nod and val
+// nod: an array of node number which has value
+// val: value of the node
 func BuildVec(nod []int, val []float64, vec *mat64.Vector) {
   for i,n := range nod {
     vec.SetVec(n, val[i])
   }
 }
 
+// Calculate Gaussian Quadracture of f from a to b with num Gaussian point
 func GausQuad(f func(float64) float64, a, b float64, num int) float64 {
   pt, w := getGausQuadPoint(num)
   f = changeInterval(f, a, b)
@@ -30,6 +34,7 @@ func GausQuad(f func(float64) float64, a, b float64, num int) float64 {
   return sum
 }
 
+// Get the value of Gaussian points and the weight. Currently maximun num is 4.
 func getGausQuadPoint(num int) (pt, w []float64) {
   pt = make([]float64, num)
   w = make([]float64, num)
@@ -50,12 +55,14 @@ func getGausQuadPoint(num int) (pt, w []float64) {
   return pt, w
 }
 
+// Change the interval from a, b to -1, 1
 func changeInterval(f func (float64) float64, a, b float64) func (float64) float64 {
   return func (x float64) float64 {
     return (b-a)/2 * f( (b-a)*x/2 + (a+b)/2  )
   }
 }
 
+// Initialize the force boundary condition
 func InitF (Nn int, uNod, fNod []int, fVal []float64) ([]int, []float64) {
   uNum := len(uNod)
   fNum := len(fNod)
@@ -78,6 +85,17 @@ func InitF (Nn int, uNod, fNod []int, fVal []float64) ([]int, []float64) {
   return nfNod, nfVal
 }
 
+// Linear-element FEM solver for column
+// k: local stifness matrix
+// K: global stifness matrix
+// Ne: amount of element
+// Le: lenth of each element
+// E: Young's modulus of each element
+// A: area of each element
+// u: displacement vector
+// f: force vector
+// uNod, uVal: displacement boundary condition
+// fNod, fVal: force boundary condition
 type FEMsolver1d struct {
   k, K *mat64.SymDense
   Ne int
@@ -86,6 +104,7 @@ type FEMsolver1d struct {
   uVal, fVal []float64
 }
 
+// Create a FEMsolver1d
 func NewFEMsolver1d(Ne int, Le, E, A, u, f *mat64.Vector, uNod, fNod []int, uVal, fVal []float64) *FEMsolver1d {
   k := mat64.NewSymDense(2, []float64{
     1, -1,
@@ -98,6 +117,7 @@ func NewFEMsolver1d(Ne int, Le, E, A, u, f *mat64.Vector, uNod, fNod []int, uVal
   return &FEMsolver1d{k, K, Ne, Le, E, A, u, f, uNod, fNod, uVal, fVal}
 }
 
+// Create a FEMsolver1d with const Le, E and A
 func NewFEMsolver1dConstLeEA(Ne int, Le, E, A float64, u, f *mat64.Vector, uNod, fNod []int, uVal, fVal []float64) *FEMsolver1d {
 
   LeV := mat64.NewVector(Ne+1, nil)
@@ -112,6 +132,7 @@ func NewFEMsolver1dConstLeEA(Ne int, Le, E, A float64, u, f *mat64.Vector, uNod,
   return NewFEMsolver1d(Ne, LeV, EV, AV, u, f, uNod, fNod, uVal, fVal)
 }
 
+// Return shape function of e-th element
 func (fem *FEMsolver1d) NElem(i, e int) func(float64) float64 {
   Le := fem.Le.At(e, 0)
   x1 := Le * float64(e)
@@ -121,6 +142,7 @@ func (fem *FEMsolver1d) NElem(i, e int) func(float64) float64 {
   }
 }
 
+// Return derivative shape function of e-th element
 func (fem *FEMsolver1d) BElem(i, e int) func(float64) float64 {
   Le := fem.Le.At(e, 0)
   return func(x float64) float64 {
@@ -128,6 +150,7 @@ func (fem *FEMsolver1d) BElem(i, e int) func(float64) float64 {
   }
 }
 
+// Add body force (or distributed force) b to the solver
 func (fem *FEMsolver1d) AddBodyForce(b func(float64) float64) {
   for i := 0; i < fem.Ne; i++ {
 
@@ -152,6 +175,7 @@ func (fem *FEMsolver1d) AddBodyForce(b func(float64) float64) {
   }
 }
 
+// Calculate the global K matrix
 func (fem *FEMsolver1d) CalcK() {
   fmt.Println("Calc stifness matrix ...")
   for k := 0; k < fem.Ne; k++ {
@@ -175,6 +199,7 @@ func (fem *FEMsolver1d) CalcK() {
   }
 }
 
+// Solve the problem
 func (fem *FEMsolver1d) Solve() {
   fmt.Println("Solving ...")
 
